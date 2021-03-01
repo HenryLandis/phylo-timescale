04762bb09faa92d03cdf277dd7c5864042e0fc16
HenryLandis <HenryLandis@github.com>
2021-03-01 09:19:09 -0800

    Revamp of command line code.

--- phylotimescale/__main__.py
+++ phylotimescale/__main__.py
@@ -9,7 +9,9 @@ from phylotimescale import Simulator
 
 
 def parse_command_line():
-    "Parses arguments for Simulator."
+    """
+    Parses arguments for Simulator.
+    """
 
     # Init parser and add arguments.
     parser = argparse.ArgumentParser()
@@ -68,15 +70,9 @@ def parse_command_line():
     )
     parser.add_argument(
         "--ipcoal_kwargs",
-        help = """Set params for ipcoal.  Enter as a dict.  Example:
-        {
-            "mut": 1e-8, 
-            "recomb": 1e-9, 
-            "nloci": 100, 
-            "nsites": 1000,
-        }
-        """,
-        nargs = "?",
+        help = "Set params for ipcoal.  Use quotes for args with spaces.",
+        metavar = "key=value",
+        nargs = "*",
         default = {
             "mut": 1e-8, 
             "recomb": 1e-9, 
@@ -86,33 +82,17 @@ def parse_command_line():
     )
     parser.add_argument(
         "--chronos_constraints",
-        help = """Set params for chronos.  Enter as a dict.  Example:
-        {
-            ("r0", "r1"): (1e2, 1e4),
-            ("r1", "r6"): (1e5, 1e5),        
-        }
-        """,
-        nargs = "?",
-        default = {
-            ("r0", "r1"): (1e2, 1e4),
-            ("r1", "r6"): (1e5, 1e5),        
-        }
+        help = "Set params for chronos.  Use quotes for args with spaces.",
+        nargs = 4,
+        #action = "append",
+        default = None
     )
     parser.add_argument(
         "--mb_params",
-        help = """Set params for mrbayes.  Enter as a list of lists.  Example:
-        [
-        ["test1", "r0 r1 r2 r3", "uniform(1, 10)"],
-        ["test2", "r4 r5", "uniform(1, 10)"],
-        ["test3", "r6 r7 r8 r9", "uniform(1, 10)"]
-        ],
-        """,
-        nargs = "?",
-        default = [
-        ["test1", "r0 r1 r2 r3", "uniform(1, 10)"],
-        ["test2", "r4 r5", "uniform(1, 10)"],
-        ["test3", "r6 r7 r8 r9", "uniform(1, 10)"]
-        ],
+        help = "Set params for mrbayes.",
+        nargs = 3,
+        action = "append",
+        default = None
     )
     # Look into action = append instead.  This can create a list: [constraint, tips, prior].  Then in Simulator, parse this
     # to build the constraint blocks in the Nexus file.  Use calib_string still, but with a list of 3-item lists or dicts.
@@ -136,6 +116,67 @@ def parse_command_line():
     args = parser.parse_args()
     return args
 
+def parse_pair_for_ipcoal(s):
+    """
+    Parses a key-value pair, separated by '='.
+    """
+
+    items = s.split('=')
+    key = items[0].strip()
+    if len(items) > 1:
+        value = '='.join(items[1:])
+    return (key, value)
+
+
+def parse_ipcoal_to_dict(items):
+    """
+    Parse a series of key-value pairs and return a dictionary of strings : flaots for ipcoal.
+    """
+
+    d = {}
+    if items:
+        for item in items:
+            key, value = parse_pair_for_ipcoal(item)
+            d[key] = float(value)
+    return d
+
+
+def parse_lists_for_chronos(s):
+    """
+    Parse a list of lists and return a dictionary of tuples : tuples for chronos.  Each individual call of --chronos_constraints
+    is expected to include exactly two tips and exactly two age values, of the form '(tip1)' '(min.age)' '(tip2)' '(max.age)'.  
+    Example:
+
+    ("r0", "r19", "r32") : (8460000, 12310000, 10380000), ("r8", "r25", "r35") : (18460000, 22310000, 20380000)
+    """
+    d = {}
+    print(s)
+    tip1 = ()
+    min_age = ()
+    tip2 = ()
+    max_age = ()
+    for i in s[0].split(" "):
+        tip1 = tip1 + (i ,)
+    for i in s[1].split(" "):
+        min_age = min_age + (i ,)
+    for i in s[2].split(" "):
+        tip2 = tip2 + (i ,)
+    for i in s[3].split(" "):
+        max_age = max_age + (i ,)
+    d[tip1] = min_age
+    d[tip2] = max_age
+    print(d) # This dict is formatted exactly how I want for the chronos script.  I need to edit Simulator to build the arguments
+    # directly from this setup.  Use d.get() to retrieve values.  Write a small function to enter values and get keys.
+
+    # Alternative: do list of lists like for mrbayes.  Advantage is to input clades in a sensible manner.  Also requires Simulator
+    # edits.
+    print([str(i[0]) for i in d.keys()])
+    print([str(i[1]) for i in d.keys()])
+    print([str(i[0]) for i in d.values()])
+    print([str(i[1]) for i in d.values()])
+    return d
+
+
 def main():
     """
     Run main function on parsed arguments.
@@ -144,6 +185,10 @@ def main():
     # Get arguments from command line as a dict-like object.
     args = parse_command_line()
 
+    # Build dicts for ipcoal and chronos params.
+    ipcoal_dict = parse_ipcoal_to_dict(args.ipcoal_kwargs)
+    # chronos_dict = parse_lists_for_chronos(args.chronos_constraints)
+
     # Run Simulator if positional arguments are satisfied.
     if args.tree and args.reps:
         sim = Simulator(
@@ -151,7 +196,7 @@ def main():
         reps = args.reps,
         min_Ne = args.min_Ne,
         max_Ne = args.max_Ne,
-        ipcoal_kwargs = args.ipcoal_kwargs,
+        ipcoal_kwargs = ipcoal_dict,
         chronos_constraints = args.chronos_constraints,
         mb_params = args.mb_params,
         mb_treeagepr = args.mb_treeagepr,
